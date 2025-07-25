5857e364249d69d4210320a53c7d743faf11d0d7
Ethan Cao <ethancao0104@gmail.com>
2025-07-25 14:00:01 -0700

    Removed frontend Spotify API calls (moving this to the backend)

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,27 +1,10 @@
-import { generateCodeChallenge, generateCodeVerifier } from "./utils/pkce";
 import "./styles/Login.css";
-
-const clientId = "b470ddb998e941ea8cca13ae01930a27";
-const redirectUri = "http://127.0.0.1:5173/callback";
-const scope = "user-top-read";
 
 function Login() {
   const handleLogin = async () => {
-    const verifier = generateCodeVerifier();
-    localStorage.setItem("verifier", verifier);
-
-    const challenge = await generateCodeChallenge(verifier);
-
-    const authUrl =
-      `https://accounts.spotify.com/authorize` +
-      `?client_id=${clientId}` +
-      `&response_type=code` +
-      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-      `&scope=${encodeURIComponent(scope)}` +
-      `&code_challenge_method=S256` +
-      `&code_challenge=${challenge}`;
-
-    window.location = authUrl;
+    const res = await fetch("http://localhost:5000/auth/login");
+    const data = await res.json();
+    window.location.href = data.authUrl;
   };
 
   return (

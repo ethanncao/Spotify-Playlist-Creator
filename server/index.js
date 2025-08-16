3ca418f1409583a3bef93ae716e0ed5e462271ab
Ethan Cao <ethancao0104@gmail.com>
2025-08-15 21:34:51 -0700

    Changed CORS to allow Vercel

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const spotifyRoutes = require("./routes/spotify");
 dotenv.config(); // loads our .env file
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://spotify-playlist-creator-phi.vercel.app",
+  ],
   methods: ["GET", "POST"],
 };
 
